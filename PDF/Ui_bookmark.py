daae71ee2b8fe48b7db0724914e86290a9eefc28
barret ren <barret.ren@outlook.com>
2019-06-19 22:16:02 +0800

    add QT UI files

---
+++ PDF/Ui_bookmark.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'c:\Users\Barret\Documents\Codes\PythonScripts\PDF\bookmark.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(60, 60, 72, 15))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(60, 120, 91, 16))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(60, 210, 91, 16))
+        self.label_3.setObjectName("label_3")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(150, 60, 381, 21))
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(150, 120, 381, 21))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_3.setGeometry(QtCore.QRect(150, 210, 381, 21))
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.toolButton = QtWidgets.QToolButton(self.centralwidget)
+        self.toolButton.setGeometry(QtCore.QRect(580, 60, 47, 21))
+        self.toolButton.setObjectName("toolButton")
+        self.toolButton_2 = QtWidgets.QToolButton(self.centralwidget)
+        self.toolButton_2.setGeometry(QtCore.QRect(580, 120, 47, 21))
+        self.toolButton_2.setObjectName("toolButton_2")
+        self.toolButton_3 = QtWidgets.QToolButton(self.centralwidget)
+        self.toolButton_3.setGeometry(QtCore.QRect(580, 210, 47, 21))
+        self.toolButton_3.setObjectName("toolButton_3")
+        self.buttonBox = QtWidgets.QDialogButtonBox(self.centralwidget)
+        self.buttonBox.setGeometry(QtCore.QRect(260, 380, 193, 28))
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel
+                                          | QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName("buttonBox")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "PDF文件"))
+        self.label_2.setText(_translate("MainWindow", "目录txt文件"))
+        self.label_3.setText(_translate("MainWindow", "输出文件"))
+        self.toolButton.setText(_translate("MainWindow", "..."))
+        self.toolButton_2.setText(_translate("MainWindow", "..."))
+        self.toolButton_3.setText(_translate("MainWindow", "..."))
--- PDF/addBookmarkToPDF.py
+++ PDF/addBookmarkToPDF.py
@@ -1,8 +1,9 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 # 封装的PDF文档书签生成工具，需要提供PDF文件和书签TXT文件作为输入
 # 目前支持三级目录分级
-from PyPDF2 import PdfFileReader as reader,PdfFileWriter as writer
+from PyPDF2 import PdfFileReader as reader, PdfFileWriter as writer
 import os
+
 
 class PDFHandleMode(object):
     '''
@@ -13,11 +14,13 @@ class PDFHandleMode(object):
     # 仅保留源PDF文件的页面内容，在此基础上修改
     NEWLY = 'newly'
 
+
 class MyPDFHandler(object):
     '''
     封装的PDF文件处理类
     '''
-    def __init__(self,pdf_file_path,mode = PDFHandleMode.COPY):
+
+    def __init__(self, pdf_file_path, mode=PDFHandleMode.COPY):
         '''
         用一个PDF文件初始化
         :param pdf_file_path: PDF文件路径
@@ -41,7 +44,7 @@ class MyPDFHandler(object):
                 page = self.__pdf.getPage(idx)
                 self.__writeable_pdf.insertPage(page, idx)
 
-    def save2file(self,new_file_name):
+    def save2file(self, new_file_name):
         '''
         将修改后的PDF保存成文件
         :param new_file_name: 新文件名，不要和原文件名相同
@@ -52,7 +55,12 @@ class MyPDFHandler(object):
             self.__writeable_pdf.write(fout)
         print('save2file success! new file is: {0}'.format(new_file_name))
 
-    def add_one_bookmark(self,title,page,parent = None, color = None,fit = '/Fit'):
+    def add_one_bookmark(self,
+                         title,
+                         page,
+                         parent=None,
+                         color=None,
+                         fit='/Fit'):
         '''
         往PDF文件中添加单条书签，并且保存为一个新的PDF文件
         :param str title: 书签标题
@@ -63,11 +71,15 @@ class MyPDFHandler(object):
         :param str fit: 跳转到书签页后的缩放方式
         :return: None
         '''
-        bookmark = self.__writeable_pdf.addBookmark(title,page - 1,parent = parent,color = color,fit = fit)
+        bookmark = self.__writeable_pdf.addBookmark(title,
+                                                    page - 1,
+                                                    parent=parent,
+                                                    color=color,
+                                                    fit=fit)
         print('add_one_bookmark success! bookmark title is: {0}'.format(title))
         return bookmark
 
-    def add_bookmarks(self,bookmarks):
+    def add_bookmarks(self, bookmarks):
         '''
         批量添加书签
         :param bookmarks: 书签元组列表，其中的页码表示的是PDF中的绝对页码，值为1表示第一页
@@ -75,16 +87,17 @@ class MyPDFHandler(object):
         '''
         l1 = None
         l2 = None
-        for level,title,page in bookmarks:
+        for level, title, page in bookmarks:
             if (level == 1):
-                l1 = self.add_one_bookmark(title,page)
+                l1 = self.add_one_bookmark(title, page)
             elif (level == 2):
-                l2 = self.add_one_bookmark(title,page, l1)
+                l2 = self.add_one_bookmark(title, page, l1)
             else:
-                self.add_one_bookmark(title,page, l2)
-        print('add_bookmarks success! add {0} pieces of bookmarks to PDF file'.format(len(bookmarks)))
+                self.add_one_bookmark(title, page, l2)
+        print('add_bookmarks success! add {0} pieces of bookmarks to PDF file'.
+              format(len(bookmarks)))
 
-    def read_bookmarks_from_txt(self,txt_file_path,page_offset = 0):
+    def read_bookmarks_from_txt(self, txt_file_path, page_offset=0):
         '''
         从文本文件中读取书签列表
         文本文件有若干行，每行一个书签，内容格式为：
@@ -95,7 +108,7 @@ class MyPDFHandler(object):
         :return: 书签列表
         '''
         bookmarks = []
-        with open(txt_file_path,'r',encoding='utf-8') as fin:
+        with open(txt_file_path, 'r', encoding='utf-8') as fin:
             for line in fin:
                 line = line.rstrip()
                 if not line:
@@ -105,7 +118,7 @@ class MyPDFHandler(object):
                 try:
                     chapter = line.split(' ')[0].rstrip()
                     level = len(chapter.split('.'))
-                    
+
                     title = line.split('@')[0].rstrip()
                     page = line.split('@')[1].strip()
                 except IndexError as msg:
@@ -121,21 +134,27 @@ class MyPDFHandler(object):
 
         return bookmarks
 
-    def add_bookmarks_by_read_txt(self,txt_file_path,page_offset = 0):
+    def add_bookmarks_by_read_txt(self, txt_file_path, page_offset=0):
         '''
         通过读取书签列表信息文本文件，将书签批量添加到PDF文件中
         :param txt_file_path: 书签列表信息文本文件
         :param page_offset: 页码便宜量，为0或正数，即由于封面、目录等页面的存在，在PDF中实际的绝对页码比在目录中写的页码多出的差值
         :return: None
         '''
-        bookmarks = self.read_bookmarks_from_txt(txt_file_path,page_offset)
+        bookmarks = self.read_bookmarks_from_txt(txt_file_path, page_offset)
         self.add_bookmarks(bookmarks)
         print('add_bookmarks_by_read_txt success!')
 
+
 def main():
-    pdf_handler = MyPDFHandler(u'C:/N-20L6PF1PYK28-Data/barretr/Desktop/TCPIP详解卷一.pdf',PDFHandleMode.NEWLY)
-    pdf_handler.add_bookmarks_by_read_txt('C:/N-20L6PF1PYK28-Data/barretr/Desktop/1.txt')
-    pdf_handler.save2file(u'C:/N-20L6PF1PYK28-Data/barretr/Desktop/TCPIP详解卷一-目录书签版.pdf')
+    pdf_handler = MyPDFHandler(
+        u'C:/N-20L6PF1PYK28-Data/barretr/Desktop/TCPIP详解卷一.pdf',
+        PDFHandleMode.NEWLY)
+    pdf_handler.add_bookmarks_by_read_txt(
+        'C:/N-20L6PF1PYK28-Data/barretr/Desktop/1.txt')
+    pdf_handler.save2file(
+        u'C:/N-20L6PF1PYK28-Data/barretr/Desktop/TCPIP详解卷一-目录书签版.pdf')
+
 
 if __name__ == '__main__':
     main()
