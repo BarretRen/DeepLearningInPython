75ab6a4c644dd186f75f677f66dcb0d193f7e9d5
barret ren <barret.ren@outlook.com>
2019-08-06 23:36:00 +0800

    多输入和多输出模型构建

--- deep learning/23_api_multi_input.py
+++ deep learning/23_api_multi_input.py
@@ -2,6 +2,8 @@
 from keras.models import Model
 from keras import layers
 from keras import Input
+from keras import utils
+import numpy as np
 
 # 定义序列的大小
 text_size = 10000
@@ -17,3 +19,26 @@ encoded_text = layers.LSTM(32)(embedded_text)
 # 对问题输入执行相同的操作
 question_input = Input(shape=(None, ), dtype='int32', name='question')
 embedded_question = layers.Embedding(question_size, 32)(question_input)
+encoded_question = layers.LSTM(16)(embedded_question)
+
+# 将编码后的两个输入连接在一起
+conbinded = layers.concatenate([encoded_text, encoded_question], axis=-1)
+# 添加分类器
+answer = layers.Dense(answer_size, activation='softmax')(conbinded)
+
+# 实例化模型，指定输入和输出
+model = Model([text_input, question_input], answer)
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['acc'])
+
+# 生成虚拟数据，进行训练
+num_samples = 1000
+max_length = 100
+
+text = np.random.randint(1, text_size, size=(num_samples, max_length))
+question = np.random.randint(1, question_size, size=(num_samples, max_length))
+answers = np.random.randint(answer_size, size=(num_samples))
+answers = utils.to_categorical(answers, answer_size)  # answers转化为one-hot编码
+
+model.fit([text, question], answers, epochs=10, batch_size=128)  # 参数要与模型实例化时一致
